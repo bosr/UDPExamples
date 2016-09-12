3e03d6e79f8444321cf03818fbc1137923cdeb3d
Romain Bossart <romain.bossart@fastmail.com>
2016-09-12 22:34:49 +0200

    add .swift-version for 3.0-GM-CANDIDATE

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "UDPExamples",
-	dependencies: [
-		.Package(url: "https://github.com/VeniceX/UDP.git", majorVersion: 0, minor: 13)
-	]
+    dependencies: [
+        .Package(url: "https://github.com/VeniceX/UDP.git", majorVersion: 0, minor: 13)
+    ]
 )
--- Sources/basic-echo-example/main.swift
+++ Sources/basic-echo-example/main.swift
@@ -8,31 +8,31 @@ let serverPort = 5150
 let clientPort = 5151
 
 co {
-	do {
-		// create an echo server on localhost:5150
-		let socket = try UDPSocket(ip: IP(port: serverPort))
-		print("UDPSocket listening at 127.0.0.1:\(serverPort)")
+    do {
+        // create an echo server on localhost:5150
+        let socket = try UDPSocket(ip: IP(port: serverPort))
+        print("UDPSocket listening at 127.0.0.1:\(serverPort)")
 
-		// send status for the client to connect
-		statusCh.send(true)
+        // send status for the client to connect
+        statusCh.send(true)
 
-		while true {
-			// waits for an incoming connection, receives 1024 bytes, sends them back
-			let (data, sourceIP) = try socket.receive(upTo: 1024)
-			print("server side: received: \(data) from ip: \(sourceIP)")
-			let destinationIP = sourceIP
-			try socket.send(data, ip: destinationIP)
-		}
-	} catch {
-		print(error)
-		statusCh.send(false)
+        while true {
+            // waits for an incoming connection, receives 1024 bytes, sends them back
+            let (data, sourceIP) = try socket.receive(upTo: 1024)
+            print("server side: received: \(data) from ip: \(sourceIP)")
+            let destinationIP = sourceIP
+            try socket.send(data, ip: destinationIP)
+        }
+    } catch {
+        print(error)
+        statusCh.send(false)
   }
 }
 
 // wait for the server to start
 guard statusCh.receive()! else {
-	print("Server could not start")
-	exit(1)
+    print("Server could not start")
+    exit(1)
 }
 
 print("creating the client")
